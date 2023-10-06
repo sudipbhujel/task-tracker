9dbca2c472adf7f515e381ac84c86c0eb67db05e
Sudip Bhujel <devsudipbhujel@gmail.com>
2023-10-06 15:59:46 +0545

    feat(logo): add logo

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Button, buttonVariants } from '../ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Separator } from '../ui/separator';
+import { ReaderIcon } from '@radix-ui/react-icons';
 
 interface NavbarProps {}
 
@@ -12,7 +13,12 @@ const Navbar: FC<NavbarProps> = () => {
   return (
     <>
       <nav className="flex justify-between items-center py-1">
-        <h1 className="text-xl font-bold">Task Tracker</h1>
+        {/* <h1 className="text-xl font-bold">Task Tracker</h1> */}
+        {/* <img src="logo.png" alt="Logo" className="h-8 w-8 scale-150" /> */}
+        <div className="flex space-x-1 items-center">
+          <ReaderIcon className="h-6 w-6" />
+          <p>Task Tracker</p>
+        </div>
 
         {!user && (
           <Link to="/login" className={buttonVariants()}>
